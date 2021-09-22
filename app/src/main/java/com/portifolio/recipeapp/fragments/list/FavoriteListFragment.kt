68b5b69a9cf001ade01eb24b417fed172aebf002
joaogomes <joaoguilherme102030@gmail.com>
2021-09-22 16:04:31 -0300

    Initial commit Dependencies: Navigation LiveData Coroutines Retrofit Room DataStore Dagger Hilt Gson Coil Jsoup Shimmer

---
+++ app/src/main/java/com/portifolio/recipeapp/fragments/list/FavoriteListFragment.kt
@@ -0,0 +1,20 @@
+package com.portifolio.recipeapp.fragments.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.portifolio.recipeapp.R
+
+class FavoriteListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorite_list, container, false)
+    }
+
+}
