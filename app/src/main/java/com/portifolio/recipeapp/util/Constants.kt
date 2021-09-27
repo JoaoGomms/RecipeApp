93503af9ce0477e9673f8753dbb0afcf2af1a8ac
joaogomes <joaoguilherme102030@gmail.com>
2021-09-26 23:34:54 -0300

    Create Constants

---
+++ app/src/main/java/com/portifolio/recipeapp/util/Constants.kt
@@ -0,0 +1,12 @@
+package com.portifolio.recipeapp.util
+
+class Constants {
+
+    companion object {
+
+        const val API_KEY = "44ff3a5cac554be294952fa18271c3a2"
+        const val BASE_URL = "https://api.spoonacular.com/"
+
+    }
+
+}
