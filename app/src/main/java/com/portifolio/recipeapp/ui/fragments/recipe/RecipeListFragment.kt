175a0f17006fa356f5710f9bd63457a4e5a02e98
joaogomes <joaoguilherme102030@gmail.com>
2021-09-30 00:13:01 -0300

    Refactor API filters to constants and apply changes on a new ViewModel to hold that data

--- app/src/main/java/com/portifolio/recipeapp/ui/fragments/recipe/RecipeListFragment.kt
+++ app/src/main/java/com/portifolio/recipeapp/ui/fragments/recipe/RecipeListFragment.kt
@@ -10,9 +10,9 @@ import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.portifolio.recipeapp.adapters.RecipesAdapter
 import com.portifolio.recipeapp.databinding.FragmentRecipeListBinding
-import com.portifolio.recipeapp.util.Constants.Companion.API_KEY
 import com.portifolio.recipeapp.util.NetworkResult
 import com.portifolio.recipeapp.viewmodel.MainViewModel
+import com.portifolio.recipeapp.viewmodel.RecipesViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,7 +22,8 @@ class RecipeListFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val recipesAdapter by lazy { RecipesAdapter() }
-    private val viewModel : MainViewModel by viewModels()
+    private val mainViewModel : MainViewModel by viewModels()
+    private val recipesViewModel : RecipesViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,8 +39,8 @@ class RecipeListFragment : Fragment() {
     }
 
     private fun requestApiData(){
-        viewModel.getRecipes(applyQueries())
-        viewModel.getRecipesResponse().observe(viewLifecycleOwner, {
+        mainViewModel.getRecipes(recipesViewModel.applyQueries())
+        mainViewModel.getRecipesResponse().observe(viewLifecycleOwner, {
             response -> when(response){
                 is NetworkResult.Success -> {
                     hideShimmerEffect()
@@ -54,19 +55,6 @@ class RecipeListFragment : Fragment() {
                 }
             }
         })
-    }
-
-    private fun applyQueries(): HashMap<String, String>{
-        val queries: HashMap<String, String> = HashMap()
-
-        queries["number"] = "50"
-        queries["apiKey"] = API_KEY
-        queries["type"] = "snack"
-        queries["diet"] = "vegan"
-        queries["addRecipeInformation"] = "true"
-        queries["fillIngredients"] = "true"
-
-        return queries
     }
 
 
--- app/src/main/java/com/portifolio/recipeapp/util/Constants.kt
+++ app/src/main/java/com/portifolio/recipeapp/util/Constants.kt
@@ -7,6 +7,14 @@ class Constants {
         const val API_KEY = "44ff3a5cac554be294952fa18271c3a2"
         const val BASE_URL = "https://api.spoonacular.com/"
 
+        //API Query KeySet
+        const val QUERY_NUMBER = "number"
+        const val QUERY_API_KEY = "apiKey"
+        const val QUERY_TYPE = "type"
+        const val QUERY_DIET = "diet"
+        const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
+        const val QUERY_FILL_INGREDIENTS = "fillIngredients"
+
     }
 
 }
