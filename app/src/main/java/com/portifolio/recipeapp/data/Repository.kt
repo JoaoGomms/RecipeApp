557511a8632e6afc5ebfe6c33560f8318580277c
joaogomes <joaoguilherme102030@gmail.com>
2021-10-01 20:03:18 -0300

    Implement LocalDataSource Add local on Repository

--- app/src/main/java/com/portifolio/recipeapp/data/Repository.kt
+++ app/src/main/java/com/portifolio/recipeapp/data/Repository.kt
@@ -1,5 +1,6 @@
 package com.portifolio.recipeapp.data
 
+import com.portifolio.recipeapp.data.local.LocalDataSource
 import com.portifolio.recipeapp.data.remote.RemoteDataSource
 import dagger.hilt.android.scopes.ActivityRetainedScoped
 import javax.inject.Inject
@@ -8,8 +9,10 @@ import javax.inject.Inject
 @ActivityRetainedScoped
 class Repository @Inject constructor(
     remoteDataSource: RemoteDataSource,
+    localDataSource: LocalDataSource,
 ){
 
     val remote = remoteDataSource
+    val local = localDataSource
 
 }
--- app/src/main/java/com/portifolio/recipeapp/data/local/LocalDataSource.kt
+++ app/src/main/java/com/portifolio/recipeapp/data/local/LocalDataSource.kt
@@ -1,6 +1,18 @@
 package com.portifolio.recipeapp.data.local
 
-class LocalDataSource{
+import com.portifolio.recipeapp.data.db.dao.RecipesDao
+import com.portifolio.recipeapp.data.db.entity.RecipesEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
+class LocalDataSource @Inject constructor(private val dao: RecipesDao){
+
+    fun readDatabase(): Flow<List<RecipesEntity>> {
+        return dao.readRecipes()
+    }
+
+    suspend fun insertRecipes(recipes: RecipesEntity){
+        dao.insertRecipes(recipes)
+    }
 
 }
